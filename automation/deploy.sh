a4a7e5926640b1fd5b22faed92678aec39e2e004
shayle <shayle@checkpoint.com>
2025-04-07 00:15:10 +0300

    fix script

--- automation/deploy.sh
+++ automation/deploy.sh
@@ -18,15 +18,14 @@ function configure_nginx(){
     tee /etc/nginx/sites-available/default > /dev/null << 'EOF'
 server {
     listen 80;
-    server_name localhost 127.0.0.1;
+    server_name localhost;
 
     location / {
-        try_files \$uri \$uri/ =404;
         proxy_pass http://127.0.0.1:5000;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
     }
 }
 EOF
